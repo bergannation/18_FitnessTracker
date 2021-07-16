8ec34d05680c7b395d54b9b0a2a615e811422009
Travis Brown <bergannation@gmail.com>
2021-07-16 14:08:48 -0500

    adding files

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 5500;
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Mongoose Database");
+});
+
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on: ${PORT}!`);
+});
